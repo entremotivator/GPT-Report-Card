5db3a02749c2c40326fff73d3c05a5cd2da14e20
entremotivator <104661532+entremotivator@users.noreply.github.com>
2023-12-04 01:04:05 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,11 +4,10 @@ import plotly.express as px  # pip install plotly-express
 import base64  # Standard Python Module
 from io import StringIO, BytesIO  # Standard Python Module
 
-
 def generate_excel_download_link(df):
     # Credit Excel: https://discuss.streamlit.io/t/how-to-add-a-download-excel-csv-function-to-a-button/4474/5
     towrite = BytesIO()
-    df.to_excel(towrite, encoding="utf-8", index=False, header=True)  # write to BytesIO buffer
+    df.to_excel(towrite, encoding="utf-8", index=False, header=True, engine='xlsxwriter')  # Specify 'xlsxwriter' engine
     towrite.seek(0)  # reset pointer
     b64 = base64.b64encode(towrite.read()).decode()
     href = f'<a href="data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64,{b64}" download="data_download.xlsx">Download Excel File</a>'
@@ -23,38 +22,42 @@ def generate_html_download_link(fig):
     href = f'<a href="data:text/html;charset=utf-8;base64, {b64}" download="plot.html">Download Plot</a>'
     return st.markdown(href, unsafe_allow_html=True)
 
+def main():
+    st.set_page_config(page_title='Excel Plotter')
+    st.title('Excel Plotter 📈')
+    st.subheader('Feed me with your Excel file')
 
-st.set_page_config(page_title='Excel Plotter')
-st.title('Excel Plotter 📈')
-st.subheader('Feed me with your Excel file')
+    uploaded_file = st.file_uploader('Choose a XLSX file', type='xlsx')
+    if uploaded_file:
+        st.markdown('---')
+        df = pd.read_excel(uploaded_file, engine='openpyxl')
+        st.dataframe(df)
+        groupby_column = st.selectbox(
+            'What would you like to analyse?',
+            ('Ship Mode', 'Segment', 'Category', 'Sub-Category'),
+        )
 
-uploaded_file = st.file_uploader('Choose a XLSX file', type='xlsx')
-if uploaded_file:
-    st.markdown('---')
-    df = pd.read_excel(uploaded_file, engine='openpyxl')
-    st.dataframe(df)
-    groupby_column = st.selectbox(
-        'What would you like to analyse?',
-        ('Ship Mode', 'Segment', 'Category', 'Sub-Category'),
-    )
+        # -- GROUP DATAFRAME
+        output_columns = ['Sales', 'Profit']
+        df_grouped = df.groupby(by=[groupby_column], as_index=False)[output_columns].sum()
 
-    # -- GROUP DATAFRAME
-    output_columns = ['Sales', 'Profit']
-    df_grouped = df.groupby(by=[groupby_column], as_index=False)[output_columns].sum()
+        # -- PLOT DATAFRAME
+        fig = px.bar(
+            df_grouped,
+            x=groupby_column,
+            y='Sales',
+            color='Profit',
+            color_continuous_scale=['red', 'yellow', 'green'],
+            template='plotly_white',
+            title=f'<b>Sales & Profit by {groupby_column}</b>'
+        )
+        st.plotly_chart(fig)
 
-    # -- PLOT DATAFRAME
-    fig = px.bar(
-        df_grouped,
-        x=groupby_column,
-        y='Sales',
-        color='Profit',
-        color_continuous_scale=['red', 'yellow', 'green'],
-        template='plotly_white',
-        title=f'<b>Sales & Profit by {groupby_column}</b>'
-    )
-    st.plotly_chart(fig)
+        # -- DOWNLOAD SECTION
+        st.subheader('Downloads:')
+        generate_excel_download_link(df_grouped)
+        generate_html_download_link(fig)
 
-    # -- DOWNLOAD SECTION
-    st.subheader('Downloads:')
-    generate_excel_download_link(df_grouped)
-    generate_html_download_link(fig)
+if __name__ == "__main__":
+    main()
+
