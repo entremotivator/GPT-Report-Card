9063338f32b176685f9a7d51c9ce07f780e6c17f
entremotivator <104661532+entremotivator@users.noreply.github.com>
2023-12-03 23:51:45 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,60 +1,131 @@
-import streamlit as st  # pip install streamlit
-import pandas as pd  # pip install pandas
-import plotly.express as px  # pip install plotly-express
-import base64  # Standard Python Module
-from io import StringIO, BytesIO  # Standard Python Module
+import streamlit as st
+import pandas as pd
+import datetime
+from PIL import Image
+import plotly.express as px
+import plotly.graph_objects as go
 
+# reading the data from excel file
+df = pd.read_excel("Adidas.xlsx")
+st.set_page_config(layout="wide")
+st.markdown('<style>div.block-container{padding-top:1rem;}</style>', unsafe_allow_html=True)
+image = Image.open('adidas-logo.jpg')
 
-def generate_excel_download_link(df):
-    # Credit Excel: https://discuss.streamlit.io/t/how-to-add-a-download-excel-csv-function-to-a-button/4474/5
-    towrite = BytesIO()
-    df.to_excel(towrite, encoding="utf-8", index=False, header=True)  # write to BytesIO buffer
-    towrite.seek(0)  # reset pointer
-    b64 = base64.b64encode(towrite.read()).decode()
-    href = f'<a href="data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64,{b64}" download="data_download.xlsx">Download Excel File</a>'
-    return st.markdown(href, unsafe_allow_html=True)
+col1, col2 = st.columns([0.1,0.9])
+with col1:
+    st.image(image,width=100)
 
-def generate_html_download_link(fig):
-    # Credit Plotly: https://discuss.streamlit.io/t/download-plotly-plot-as-html/4426/2
-    towrite = StringIO()
-    fig.write_html(towrite, include_plotlyjs="cdn")
-    towrite = BytesIO(towrite.getvalue().encode())
-    b64 = base64.b64encode(towrite.read()).decode()
-    href = f'<a href="data:text/html;charset=utf-8;base64, {b64}" download="plot.html">Download Plot</a>'
-    return st.markdown(href, unsafe_allow_html=True)
+html_title = """
+    <style>
+    .title-test {
+    font-weight:bold;
+    padding:5px;
+    border-radius:6px;
+    }
+    </style>
+    <center><h1 class="title-test">Adidas Interactive Sales Dashboard</h1></center>"""
+with col2:
+    st.markdown(html_title, unsafe_allow_html=True)
 
+col3, col4, col5 = st.columns([0.1,0.45,0.45])
+with col3:
+    box_date = str(datetime.datetime.now().strftime("%d %B %Y"))
+    st.write(f"Last updated by:  \n {box_date}")
 
-st.set_page_config(page_title='Excel Plotter')
-st.title('Excel Plotter 📈')
-st.subheader('Feed me with your Excel file')
+with col4:
+    fig = px.bar(df, x = "Retailer", y = "TotalSales", labels={"TotalSales" : "Total Sales {$}"},
+                 title = "Total Sales by Retailer", hover_data=["TotalSales"],
+                 template="gridon",height=500)
+    st.plotly_chart(fig,use_container_width=True)
 
-uploaded_file = st.file_uploader('Choose a XLSX file', type='xlsx')
-if uploaded_file:
-    st.markdown('---')
-    df = pd.read_excel(uploaded_file, engine='openpyxl')
-    st.dataframe(df)
-    groupby_column = st.selectbox(
-        'What would you like to analyse?',
-        ('Ship Mode', 'Segment', 'Category', 'Sub-Category'),
-    )
+_, view1, dwn1, view2, dwn2 = st.columns([0.15,0.20,0.20,0.20,0.20])
+with view1:
+    expander = st.expander("Retailer wise Sales")
+    data = df[["Retailer","TotalSales"]].groupby(by="Retailer")["TotalSales"].sum()
+    expander.write(data)
+with dwn1:
+    st.download_button("Get Data", data = data.to_csv().encode("utf-8"),
+                       file_name="RetailerSales.csv", mime="text/csv")
 
-    # -- GROUP DATAFRAME
-    output_columns = ['Sales', 'Profit']
-    df_grouped = df.groupby(by=[groupby_column], as_index=False)[output_columns].sum()
+df["Month_Year"] = df["InvoiceDate"].dt.strftime("%b'%y")
+result = df.groupby(by = df["Month_Year"])["TotalSales"].sum().reset_index()
 
-    # -- PLOT DATAFRAME
-    fig = px.bar(
-        df_grouped,
-        x=groupby_column,
-        y='Sales',
-        color='Profit',
-        color_continuous_scale=['red', 'yellow', 'green'],
-        template='plotly_white',
-        title=f'<b>Sales & Profit by {groupby_column}</b>'
-    )
-    st.plotly_chart(fig)
+with col5:
+    fig1 = px.line(result, x = "Month_Year", y = "TotalSales", title="Total Sales Over Time",
+                   template="gridon")
+    st.plotly_chart(fig1,use_container_width=True)
 
-    # -- DOWNLOAD SECTION
-    st.subheader('Downloads:')
-    generate_excel_download_link(df_grouped)
-    generate_html_download_link(fig)
+with view2:
+    expander = st.expander("Monthly Sales")
+    data = result
+    expander.write(data)
+with dwn2:
+    st.download_button("Get Data", data = result.to_csv().encode("utf-8"),
+                       file_name="Monthly Sales.csv", mime="text/csv")
+    
+st.divider()
+
+result1 = df.groupby(by="State")[["TotalSales","UnitsSold"]].sum().reset_index()
+
+# add the units sold as a line chart on a secondary y-axis
+fig3 = go.Figure()
+fig3.add_trace(go.Bar(x = result1["State"], y = result1["TotalSales"], name = "Total Sales"))
+fig3.add_trace(go.Scatter(x=result1["State"], y = result1["UnitsSold"], mode = "lines",
+                          name ="Units Sold", yaxis="y2"))
+fig3.update_layout(
+    title = "Total Sales and Units Sold by State",
+    xaxis = dict(title="State"),
+    yaxis = dict(title="Total Sales", showgrid = False),
+    yaxis2 = dict(title="Units Sold", overlaying = "y", side = "right"),
+    template = "gridon",
+    legend = dict(x=1,y=1.1)
+)
+_, col6 = st.columns([0.1,1])
+with col6:
+    st.plotly_chart(fig3,use_container_width=True)
+
+_, view3, dwn3 = st.columns([0.5,0.45,0.45])
+with view3:
+    expander = st.expander("View Data for Sales by Units Sold")
+    expander.write(result1)
+with dwn3:
+    st.download_button("Get Data", data = result1.to_csv().encode("utf-8"), 
+                       file_name = "Sales_by_UnitsSold.csv", mime="text/csv")
+st.divider()
+
+_, col7 = st.columns([0.1,1])
+treemap = df[["Region","City","TotalSales"]].groupby(by = ["Region","City"])["TotalSales"].sum().reset_index()
+
+def format_sales(value):
+    if value >= 0:
+        return '{:.2f} Lakh'.format(value / 1_000_00)
+
+treemap["TotalSales (Formatted)"] = treemap["TotalSales"].apply(format_sales)
+
+fig4 = px.treemap(treemap, path = ["Region","City"], values = "TotalSales",
+                  hover_name = "TotalSales (Formatted)",
+                  hover_data = ["TotalSales (Formatted)"],
+                  color = "City", height = 700, width = 600)
+fig4.update_traces(textinfo="label+value")
+
+with col7:
+    st.subheader(":point_right: Total Sales by Region and City in Treemap")
+    st.plotly_chart(fig4,use_container_width=True)
+
+_, view4, dwn4 = st.columns([0.5,0.45,0.45])
+with view4:
+    result2 = df[["Region","City","TotalSales"]].groupby(by=["Region","City"])["TotalSales"].sum()
+    expander = st.expander("View data for Total Sales by Region and City")
+    expander.write(result2)
+with dwn4:
+    st.download_button("Get Data", data = result2.to_csv().encode("utf-8"),
+                                        file_name="Sales_by_Region.csv", mime="text.csv")
+
+_,view5, dwn5 = st.columns([0.5,0.45,0.45])
+with view5:
+    expander = st.expander("View Sales Raw Data")
+    expander.write(df)
+with dwn5:
+    st.download_button("Get Raw Data", data = df.to_csv().encode("utf-8"),
+                       file_name = "SalesRawData.csv", mime="text/csv")
+st.divider()
