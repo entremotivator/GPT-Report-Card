1f9308e262d4ca5fa99c975acbede296bedd1d16
entremotivator <104661532+entremotivator@users.noreply.github.com>
2023-12-03 22:51:09 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,60 +1,111 @@
-import streamlit as st
+
+import requests
+import json
 import pandas as pd
-import plotly
-from io import BytesIO, StringIO
+import logging
+import configparser
+import argparse
+import schedule
+import time
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
-# Function to generate download link for Excel file
-def generate_excel_download_link(df):
-    towrite = BytesIO()
-    df.to_excel(towrite, encoding="utf-8", index=False, header=True)
-    towrite.seek(0)
-    b64 = base64.b64encode(towrite.read()).decode()
-    href = f'<a href="data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64,{b64}" download="data_download.xlsx">Download Excel File</a>'
-    return st.markdown(href, unsafe_allow_html=True)
+# Set up logging
+logging.basicConfig(filename='/Users/donmenicohudson/Downloads/langchain_agent.log', level=logging.INFO, format='%(asctime)s %(levelname)s: %(message)s')
 
-# Function to generate download link for Plotly chart as HTML
-def generate_html_download_link(fig):
-    towrite = StringIO()
-    fig.write_html(towrite, include_plotlyjs="cdn")
-    towrite = BytesIO(towrite.getvalue().encode())
-    b64 = base64.b64encode(towrite.read()).decode()
-    href = f'<a href="data:text/html;charset=utf-8;base64, {b64}" download="plot.html">Download Plot</a>'
-    return st.markdown(href, unsafe_allow_html=True)
+# Parse command-line arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('--langsmith_endpoint', help='The Langsmith API endpoint')
+parser.add_argument('--trulens_endpoint', help='The Trulens API endpoint')
+parser.add_argument('--api_key', help='The API key')
+args = parser.parse_args()
 
-st.set_page_config(page_title='Excel Plotter')
-st.title('Excel Plotter 📈')
-st.subheader('Feed me with your Excel file')
+# Get the API endpoints and headers from the command-line arguments or the configuration file
+config = configparser.ConfigParser()
+config.read('/Users/donmenicohudson/Downloads/langchain_agent.ini')
+langsmith_api_endpoint = args.langsmith_endpoint or config.get('API', 'langsmith_endpoint')
+trulens_api_endpoint = args.trulens_endpoint or config.get('API', 'trulens_endpoint')
+headers = {
+    'Content-Type': 'application/json',
+    'Authorization': f"Bearer {args.api_key or config.get('API', 'api_key')}"
+}
 
-uploaded_file = st.file_uploader('Choose a XLSX file', type='xlsx')
-if uploaded_file:
-    st.markdown('---')
-    df = pd.read_excel(uploaded_file, engine='openpyxl')
-    st.dataframe(df)
-    groupby_column = st.selectbox(
-        'What would you like to analyze?',
-        ('Importance', 'Feelings', 'Consistency', 'Linguistic Match', 'Harmfulness', 'Content Safety',
-         'Accuracy', 'Unlabeled Criteria', 'Labeled Criteria', 'Semantic Similarity', 'Textual Similarity',
-         'Run Feedback Logging', 'Feedback Filtering', 'Dataset Exporting'),
-    )
+# Define the job that will be scheduled
+def job():
+    try:
+        # Make a GET request to the Langsmith API
+        langsmith_response = requests.get(langsmith_api_endpoint, headers=headers)
+        langsmith_response.raise_for_status()
+    except requests.exceptions.RequestException as e:
+        logging.error(f'Error making request to Langsmith API: {e}')
+        langsmith_data = {}
+    else:
+        # Parse the Langsmith API response
+        langsmith_data = langsmith_response.json()
+        logging.info('Langsmith API response parsed successfully')
 
-    # GROUP DATAFRAME
-    output_columns = ['Sales', 'Profit']
-    df_grouped = df.groupby(by=[groupby_column], as_index=False)[output_columns].sum()
+    try:
+        # Make a GET request to the Trulens API
+        trulens_response = requests.get(trulens_api_endpoint, headers=headers)
+        trulens_response.raise_for_status()
+    except requests.exceptions.RequestException as e:
+        logging.error(f'Error making request to Trulens API: {e}')
+        trulens_data = {}
+    else:
+        # Parse the Trulens API response
+        trulens_data = trulens_response.json()
+        logging.info('Trulens API response parsed successfully')
 
-    # PLOT DATAFRAME
-    fig = px.bar(
-        df_grouped,
-        x=groupby_column,
-        y='Sales',
-        color='Profit',
-        color_continuous_scale=['red', 'yellow', 'green'],
-        template='plotly_white',
-        title=f'<b>Sales & Profit by {groupby_column}</b>'
-    )
-    st.plotly_chart(fig)
+    try:
+        # Combine the Langsmith and Trulens data into a single report
+        report = pd.concat([pd.DataFrame(langsmith_data), pd.DataFrame(trulens_data)], ignore_index=True)
+    except Exception as e:
+        logging.error(f'Error combining data into report: {e}')
+        report = pd.DataFrame()
+    else:
+        logging.info('Data combined into report successfully')
 
-    # DOWNLOAD SECTION
-    st.subheader('Downloads:')
-    generate_excel_download_link(df_grouped)
-    generate_html_download_link(fig)
+    # Define the file path
+    file_path = '/Users/donmenicohudson/Downloads/report.csv'
 
+    try:
+        # Save the report as a CSV file
+        report.to_csv(file_path, index=False)
+    except Exception as e:
+        logging.error(f'Error saving report: {e}')
+    else:
+        logging.info('Report saved successfully')
+
+        # Send an email notification
+        try:
+            # Define the email parameters
+            sender = 'sender@example.com'
+            receiver = 'receiver@example.com'
+            subject = 'Report saved successfully'
+            body = f'The report has been saved as {file_path}.'
+            server = 'smtp.example.com'
+            username = 'username'
+            password = 'password'
+
+            # Create the email
+            email = MIMEMultipart()
+            email['From'] = sender
+            email['To'] = receiver
+            email['Subject'] = subject
+            email.attach(MIMEText(body, 'plain'))
+
+            # Send the email
+            with smtplib.SMTP(server) as smtp:
+                smtp.login(username, password)
+                smtp.send_message(email)
+        except Exception as e:
+            logging.error(f'Error sending email: {e}')
+
+# Schedule the job to run every hour
+schedule.every(1).hours.do(job)
+
+# Keep the script running
+while True:
+    schedule.run_pending()
+    time.sleep(1)
