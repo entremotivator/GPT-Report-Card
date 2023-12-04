949810101daef7dcb32c4775bca73f35b27d28b7
entremotivator <104661532+entremotivator@users.noreply.github.com>
2023-12-03 23:58:32 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import datetime
 from PIL import Image
-import plotly.graph_objects as go
 
 # reading the data from excel file
 df = pd.read_excel("Adidas.xlsx")
