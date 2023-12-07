a184207a483621f177b8fa0b170e20e686c8325a
entremotivator <104661532+entremotivator@users.noreply.github.com>
2023-12-07 03:17:41 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import plotly.express as px
 import base64
 from io import StringIO, BytesIO
 
-def generate_download_link(df, fig):
+def generate_download_link(df, fig, groupby_column, download_section):
     # Download as Excel
     excel_buffer = BytesIO()
     df.to_excel(excel_buffer, encoding="utf-8", index=False, header=True, engine='openpyxl')
@@ -19,7 +19,10 @@ def generate_download_link(df, fig):
     html_b64 = base64.b64encode(html_buffer.read()).decode()
     html_href = f'<a href="data:text/html;charset=utf-8;base64, {html_b64}" download="plot.html">Download HTML Plot</a>'
 
-    return st.markdown(excel_href + "&emsp;&emsp;" + html_href, unsafe_allow_html=True)
+    download_section.markdown(
+        f"Download: {excel_href} &emsp;&emsp; {html_href}", 
+        unsafe_allow_html=True
+    )
 
 def main():
     st.set_page_config(page_title='GPT Report Card')
@@ -27,51 +30,66 @@ def main():
     st.subheader('Evaluate and analyze LLM & Agent performance')
 
     uploaded_file = st.file_uploader('Choose a XLSX file', type='xlsx')
-    if uploaded_file:
-        st.markdown('---')
+    if not uploaded_file:
+        st.info("Please upload an XLSX file.")
+        return
+
+    st.markdown('---')
+    
+    try:
         df = pd.read_excel(uploaded_file, engine='openpyxl')
-        st.dataframe(df)
-        groupby_column = st.selectbox(
-            'What would you like to analyse?',
-            ('Ship Mode', 'Segment', 'Category', 'Sub-Category'),
-        )
+    except Exception as e:
+        st.error(f"Error: {e}")
+        return
 
-        # -- GROUP DATAFRAME
-        output_columns = ['Sales', 'Profit']
-        df_grouped = df.groupby(by=[groupby_column], as_index=False)[output_columns].sum()
+    st.dataframe(df)
 
-        # -- PLOT DATAFRAME
-        fig1 = px.bar(
-            df_grouped,
-            x=groupby_column,
-            y='Sales',
-            color='Profit',
-            color_continuous_scale=['red', 'yellow', 'green'],
-            template='plotly_white',
-            title=f'<b>Sales & Profit by {groupby_column}</b>'
-        )
-        st.plotly_chart(fig1)
+    groupby_column = st.selectbox(
+        'What would you like to analyse?',
+        ('Ship Mode', 'Segment', 'Category', 'Sub-Category'),
+    )
 
-        fig2 = px.pie(
-            df_grouped,
-            names=groupby_column,
-            values='Sales',
-            title=f'<b>Sales Distribution by {groupby_column}</b>'
-        )
-        st.plotly_chart(fig2)
+    # Check if the selected groupby_column exists in the DataFrame
+    if groupby_column not in df.columns:
+        st.warning(f"Warning: '{groupby_column}' column not found in the DataFrame.")
+        return
 
-        fig3 = px.scatter(
-            df_grouped,
-            x='Sales',
-            y='Profit',
-            color=groupby_column,
-            title=f'<b>Scatter Plot of Sales vs Profit</b>'
-        )
-        st.plotly_chart(fig3)
+    # -- GROUP DATAFRAME
+    output_columns = ['Sales', 'Profit']
+    df_grouped = df.groupby(by=[groupby_column], as_index=False)[output_columns].sum()
 
-        # -- DOWNLOAD SECTION
-        st.subheader('Downloads:')
-        generate_download_link(df_grouped, fig1)
+    # -- PLOT DATAFRAME
+    fig1 = px.bar(
+        df_grouped,
+        x=groupby_column,
+        y='Sales',
+        color='Profit',
+        color_continuous_scale=['red', 'yellow', 'green'],
+        template='plotly_white',
+        title=f'<b>Sales & Profit by {groupby_column}</b>'
+    )
+    st.plotly_chart(fig1)
+
+    fig2 = px.pie(
+        df_grouped,
+        names=groupby_column,
+        values='Sales',
+        title=f'<b>Sales Distribution by {groupby_column}</b>'
+    )
+    st.plotly_chart(fig2)
+
+    fig3 = px.scatter(
+        df_grouped,
+        x='Sales',
+        y='Profit',
+        color=groupby_column,
+        title=f'<b>Scatter Plot of Sales vs Profit</b>'
+    )
+    st.plotly_chart(fig3)
+
+    # -- DOWNLOAD SECTION
+    download_section = st.subheader('Downloads:')
+    generate_download_link(df_grouped, fig1, groupby_column, download_section)
 
 if __name__ == "__main__":
     main()
